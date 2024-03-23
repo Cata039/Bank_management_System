2464d3103dde4ad992654e3175cf1aa8919a8a5e
Cata039 <157022488+Cata039@users.noreply.github.com>
2024-03-23 22:40:09 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -5,9 +5,8 @@
 #define MAX_ACCOUNTS 100
 
 struct Account {
-    char name[100];
-    char surname[100];
     char iban[100];
+    char ownerName[100];
     char coin[4];
     double amount;
 };
@@ -23,7 +22,7 @@ void readAccountData() {
         exit(1);
     }
 
-    while (fscanf(file, "%s %s %s %lf", accounts[numAccounts].iban, accounts[numAccounts].name,
+    while (fscanf(file, "%s %s %s %lf", accounts[numAccounts].iban, accounts[numAccounts].ownerName,
                   accounts[numAccounts].coin, &accounts[numAccounts].amount) == 4) {
         // Increase the number of accounts read
         numAccounts++;
@@ -34,46 +33,137 @@ void readAccountData() {
 // Function to login
 void login() {
     char name[100];
-    char surname[100];
-    scanf("%s %s", name,surname);
+    scanf("%s %s", name);
+}
+
+// Function to update CSV file with account data
+void updateCSVFile() {
+    FILE *file = fopen("data.csv", "w");
+    if (file == NULL) {
+        printf("Error opening file.\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < numAccounts; i++) {
+        fprintf(file, "%s %s %s %.2lf\n", accounts[i].iban, accounts[i].ownerName, accounts[i].coin, accounts[i].amount);
+    }
+
+    fclose(file);
 }
 
 // Function to create an account
 void createAccount() {
-    char name[100];
-    char surname[100];
-    printf("Enter your name: ");
-    scanf("%s", name);
-    printf("Enter your surname: ");
-    scanf("%s", surname);
+    printf("Enter the IBAN: ");
+    scanf("%s", accounts[numAccounts].iban);
+    printf("Enter the owner's name: ");
+    scanf("%s", accounts[numAccounts].ownerName);
+    printf("Enter the coin type (e.g., EUR, USD, RON): ");
+    scanf("%s", accounts[numAccounts].coin);
+    printf("Enter the amount: ");
+    scanf("%lf", &accounts[numAccounts].amount);
+
+    numAccounts++;
     printf("Account created successfully!\n");
+
+    // Update the CSV file with the new account data
+    updateCSVFile();
 }
 
 // Function to view account data
 void viewAccountData() {
     printf("Viewing account data...\n");
     for (int i = 0; i < numAccounts; i++) {
-        printf("Name: %s %s, IBAN: %s, Coin: %s, Amount: %.2lf\n", accounts[i].name, accounts[i].surname,
-               accounts[i].iban, accounts[i].coin, accounts[i].amount);
+        printf("IBAN: %s, Owner's Name: %s, Coin: %s, Amount: %.2lf\n", accounts[i].iban, accounts[i].ownerName,
+               accounts[i].coin, accounts[i].amount);
     }
 }
 
 // Function to perform transactions
 void performTransactions() {
-    printf("Performing transactions...\n");
-    // Add code to perform transactions here
+    char sourceIban[100], destIban[100];
+    double amount;
+    printf("Enter source IBAN: ");
+    scanf("%s", sourceIban);
+    printf("Enter destination IBAN: ");
+    scanf("%s", destIban);
+    printf("Enter amount to transfer: ");
+    scanf("%lf", &amount);
+
+    // Search for source and destination accounts
+    int sourceIndex = -1, destIndex = -1;
+    for (int i = 0; i < numAccounts; i++) {
+        if (strcmp(accounts[i].iban, sourceIban) == 0) {
+            sourceIndex = i;
+        }
+        if (strcmp(accounts[i].iban, destIban) == 0) {
+            destIndex = i;
+        }
+    }
+
+    if (sourceIndex != -1 && destIndex != -1) {
+        if (accounts[sourceIndex].amount >= amount) {
+            accounts[sourceIndex].amount -= amount;
+            accounts[destIndex].amount += amount;
+            printf("Transaction successful!\n");
+            // Update the CSV file with the updated account data
+            updateCSVFile();
+        } else {
+            printf("Insufficient funds!\n");
+        }
+    } else {
+        printf("Source or destination account not found!\n");
+    }
 }
 
 // Function to edit account
 void editAccount() {
-    printf("Editing account...\n");
-    // Add code to edit account here
+    char iban[100];
+    printf("Enter the IBAN of the account to edit: ");
+    scanf("%s", iban);
+
+    int found = 0;
+    for (int i = 0; i < numAccounts; i++) {
+        if (strcmp(accounts[i].iban, iban) == 0) {
+            printf("Enter the new amount: ");
+            scanf("%lf", &accounts[i].amount);
+            printf("Enter the new coin type (e.g., EUR, USD, RON): ");
+            scanf("%s", accounts[i].coin);
+            found = 1;
+            break;
+        }
+    }
+
+    if (found) {
+        printf("Account details updated successfully!\n");
+        // Update the CSV file with the updated account data
+        updateCSVFile();
+    } else {
+        printf("Account not found!\n");
+    }
 }
 
 // Function to delete account
 void deleteAccount() {
-    printf("Deleting account...\n");
-    // Add code to delete account here
+    char deleteIban[100];
+    printf("Enter the IBAN of the account to delete: ");
+    scanf("%s", deleteIban);
+
+    for (int i = 0; i < numAccounts; i++) {
+        if (strcmp(accounts[i].iban, deleteIban) == 0) {
+            // Shift accounts to fill the gap
+            for (int j = i; j < numAccounts - 1; j++) {
+                accounts[j] = accounts[j + 1];
+            }
+            numAccounts--;
+            printf("Account deleted successfully!\n");
+
+            // Update CSV file
+            updateCSVFile();
+            return;
+        }
+    }
+
+    printf("Account with IBAN %s not found.\n", deleteIban);
 }
 
 int main() {
